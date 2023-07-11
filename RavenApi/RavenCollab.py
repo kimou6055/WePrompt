78ddc56ad5914ec396f551ad22aca3e3c960d072
kimou6055 <mohammedkarim.akkari@esprit.tn>
2023-07-11 13:59:00 +0100

    Update RavenCollab.py

--- RavenApi/RavenCollab.py
+++ RavenApi/RavenCollab.py
@@ -4,7 +4,7 @@ current_path = os.path.dirname(os.path.abspath(__file__))
 import numpy as np
 import torch
 os.environ["RWKV_JIT_ON"] = '1' 
-os.environ["RWKV_CUDA_ON"] = '0'
+os.environ["RWKV_CUDA_ON"] = '1'
 from rwkv.model import RWKV
 from rwkv.utils import PIPELINE
 from prompt_toolkit import prompt
@@ -24,7 +24,7 @@ class ChatRWKV:
         torch.backends.cudnn.allow_tf32 = True
         torch.backends.cuda.matmul.allow_tf32 = True
         #self.args.strategy = 'cuda fp16 *12 -> cuda fp16i8 *1 -> cpu fp32'
-        self.args.strategy = 'cuda fp16'
+        self.args.strategy = 'cuda fp16 -> cpu fp32'
         self.args.MODEL_NAME = 'RWKV-4-Raven-7B-v12-Eng98%-Other2%-20230521-ctx8192'
 
         self.CHAT_LANG = 'English'
