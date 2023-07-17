eee13e98fee46ded77455350486bbffc57e782c2
kimou6055 <mohammedkarim.akkari@esprit.tn>
2023-07-17 14:24:08 +0100

    a

--- RavenApi/RavenCollab.py
+++ RavenApi/RavenCollab.py
@@ -33,8 +33,8 @@ class ChatRWKV:
         np.set_printoptions(precision=4, suppress=True, linewidth=200)
         self.args = types.SimpleNamespace()
         
-        self.args.strategy = 'cuda fp16 *12 -> cuda fp16i8 *1 -> cpu fp32'
-        #self.args.strategy = 'cuda fp16'
+        #self.args.strategy = 'cuda fp16 *12 -> cuda fp16i8 *1 -> cpu fp32'
+        self.args.strategy = 'cuda fp16'
         self.args.MODEL_NAME = 'RWKV-4-Raven-7B-v12-Eng98%-Other2%-20230521-ctx8192'
         self.CHAT_LANG = 'English'
         
